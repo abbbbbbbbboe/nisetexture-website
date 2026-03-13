fd8c4aea48bebf98c9e1f6ba411f37fc491dd588
otasora <ootaoota0038@gmail.com>
2026-03-13 19:18:51 +0900

    blog-003追加

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,10 +1,13 @@
 import post000 from "./post000.js";
 import post001 from "./post001.js";
 import post002 from "./post002.js";
+import post003 from "./post003.js";
+
 
 
 export const posts = [
   // 新しいのは上に記入
+  post003,
   post002,
   post001,
   post000,
