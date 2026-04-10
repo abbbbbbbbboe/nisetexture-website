aa15948e3c24f1a054042d943ad36d275913f463
otasora <ootaoota0038@gmail.com>
2026-04-10 22:42:51 +0900

    archive_秋の隕石2026東京追加

--- js/data.js
+++ js/data.js
@@ -1,6 +1,37 @@
 const contents = {
   archive: [
     {
+      id: "AutumnMeteorite2026",
+      title: "【不採択】舞台芸術祭「秋の隕石2026東京」隕石（＝企画のこと）",
+      title_en: "[Rejected]A project for the performing arts festival “Autumn Meteorite 2026 Tokyo.",
+      date: "2026.3.2",
+      category: "competition",
+      place: "",
+      media: [
+        "workimg/competition/competition_samune-no_a.png"
+      ],
+      captions: [
+        "　"
+      ],
+      text_jp: `舞台芸術祭「秋の隕石2026東京」における、隕石（＝企画のこと）に「『リビングルーム・トレーニング キッチン#2』企画書」を応募しました。<br>
+残念ながら採択には至りませんでした。<br>
+『リビングルーム・トレーニング キッチン#1』を参考に、舞台上で動きや音、おしゃべりを順番に重ねていく構想です。応募に際して提出した企画書、応募用紙（公開用に一部編集）、会場使用プランを公開します。`,
+      text_en: `
+      We applied with the project proposal "Living Room Training: Kitchen #2" for the project for the performing arts festival “Autumn Meteorite 2026 Tokyo".
+<br>
+Unfortunately, it did not reach selection.
+<br>
+With "Living Room Training: Kitchen #1" as a reference, the concept is to layer movements, sounds, and chatting in order on the stage. We will make public the project proposal submitted for the application, the application form ( partially edited ), and the venue usage plan.
+    <br><br><br>
+    English translation by AI`,
+      text_credit: ``,
+      links: {
+        "企画書": "https://drive.google.com/file/d/1vY5obe66lsWhvU94tW9voQ4-v0_4CZ9m/view?usp=sharing",
+        "応募用紙（公開用）": "https://drive.google.com/file/d/14Pa6r8qeZr8Hsj5Sy6crBgQeW1RMzARh/view?usp=sharing",
+        "会場使用プラン": "https://drive.google.com/file/d/1cVXiEiO9-AZNXzejFJF8qDw7cXhT5WDj/view?usp=sharing"
+      }
+    },
+    {
       id: "LRTK1",
       title: "『リビングルーム・トレーニング キッチン #1』",
       title_en: "<Living Room Training: Kitchen #1>",
@@ -16,7 +47,7 @@ const contents = {
         "『リビングルーム・トレーニング キッチン#1』全編映像",
         "『リビングルーム・トレーニング キッチン#1』キャプチャ画像",
         "『リビングルーム・トレーニング キッチン#1』構造図(copy)"
-  ],
+      ],
       text_jp: `日常における何気ない行為のひとつである皿洗いをモチーフに、ジェスチャー、撮影、鑑賞、録音、模倣、それぞれが入れ子構造になるように行いながら、それらすべてを素材として重ね、映像とした作品です。
 <br><br>
 まず、皿洗いの動作を観察し、そこから一部を取り出します。取り出された動作を、キッチンではない場所に持ち出し、繰り返し演じます。その様子をビデオカメラで撮影します。そして、第三者にその映像の鑑賞と食事をしながら会話をしてもらいます。その際に、行われたおしゃべりを録音し、その内容をメンバーで真似て演じます。演じられた音声を録音し、撮影された映像と重ね、一つの映像にします。
@@ -26,7 +57,7 @@ const contents = {
 作品そのものや、鑑賞者、配信をみている人も、作品の構造のひとつとして取り込みます。
 <br><br>
 作品の構造を示した図(copy)も公開します。`,
-text_en: `This work uses dishwashing—a mundane daily activity—as its motif. It involves performing gestures, filming, viewing, recording, and imitation in a nested structure, then layering all these elements as material to create a video piece.
+      text_en: `This work uses dishwashing—a mundane daily activity—as its motif. It involves performing gestures, filming, viewing, recording, and imitation in a nested structure, then layering all these elements as material to create a video piece.
 <br><br>
 First, the dishwashing actions are observed, and a specific part is extracted. This extracted action is then taken to a location outside the kitchen and performed repeatedly. This performance is filmed with a video camera. Next, a third party is asked to view this footage while eating and conversing. The conversation that occurs during this viewing is recorded. The members then mimic and perform the content of this recorded conversation. The performed audio is recorded and layered with the filmed footage to create a single video.
 <br><br>
@@ -39,14 +70,14 @@ We also publish a diagram illustrating the work's structure.
 Translated with DeepL.com (free version)
 <br><br><br>
 English translation by AI`,
-text_credit: ``,
-       links: {
-    "全編映像": "https://youtu.be/uLCjUMLY1Co?si=MGhJ-a8kjz7owYDe",
-    "Livestream": "https://youtu.be/FvWRGIJsiaE",
-    "構造図(copy)": "https://miro.com/app/board/uXjVGLULSgI=/?share_link_id=96865327622"
-  }
-},
-   {
+      text_credit: ``,
+      links: {
+        "全編映像": "https://youtu.be/uLCjUMLY1Co?si=MGhJ-a8kjz7owYDe",
+        "Livestream": "https://youtu.be/FvWRGIJsiaE",
+        "構造図(copy)": "https://miro.com/app/board/uXjVGLULSgI=/?share_link_id=96865327622"
+      }
+    },
+    {
       id: "openWebsite",
       title: "webサイト公開",
       title_en: "Open website",
@@ -65,25 +96,25 @@ text_credit: ``,
       ],
       captions: [
         "rogo",
-    "toppage",
-    "archive1",
-    "archive2",
-    "archive3",
-    "archive4",
-    "about +profile+",
-    "ニセテ通信 +私たちの活動について+"
-    
-  ],
+        "toppage",
+        "archive1",
+        "archive2",
+        "archive3",
+        "archive4",
+        "about +profile+",
+        "ニセテ通信 +私たちの活動について+"
+
+      ],
       text_jp: `私たちのwebサイトを公開しました！これから、活動のアーカイブをここに載せていきます。<br>それに伴い、ニセテ通信に「私たちの活動について」というテキストを掲載しました。読み上げの音声もあるので、ぜひご覧ください。<br><br>また、新たにロゴも作成しました。webサイトとロゴの新設を記念してTシャツを作りました。以下のリンクからご覧いただけます。よろしければご購入ください。<a href="https://suzuri.jp/ota_sora/sections/24389">Tシャツ↗︎</a>`,
-text_en: `Our website is now live! We'll be posting archives of our activities here going forward. <br>Along with this, we've published a text titled “About Our Activities” in the Nisete Newsletter. An audio version is also available, so please take a look.<br><br>We've also created a new logo. To commemorate the launch of our website and logo, we've made T-shirts. You can view them via the link below. Please consider purchasing one if you'd like.<a href="https://suzuri.jp/ota_sora/sections/24389">Tshirt↗︎</a>`,
-text_credit: ``,
-       links: {
-    "Website": "https://nisetexture.com",
-     "Tshirt": "https://suzuri.jp/ota_sora/sections/24389",
- 
-  }
-},
-{
+      text_en: `Our website is now live! We'll be posting archives of our activities here going forward. <br>Along with this, we've published a text titled “About Our Activities” in the Nisete Newsletter. An audio version is also available, so please take a look.<br><br>We've also created a new logo. To commemorate the launch of our website and logo, we've made T-shirts. You can view them via the link below. Please consider purchasing one if you'd like.<a href="https://suzuri.jp/ota_sora/sections/24389">Tshirt↗︎</a>`,
+      text_credit: ``,
+      links: {
+        "Website": "https://nisetexture.com",
+        "Tshirt": "https://suzuri.jp/ota_sora/sections/24389",
+
+      }
+    },
+    {
       id: "YMAA2025",
       title: `【落選】やまなしメディア芸術アワード2025-26`,
       title_en: "[Failed]Yamanashi Media Arts Award2025-26",
@@ -94,7 +125,7 @@ text_credit: ``,
         "workimg/competition/competition_samune-no_b.png",
       ],
       captions: [
-    "　"
+        "　"
       ],
       text_jp: `やまなしメディア芸術アワード2025-26に応募しましたが、残念ながら入選には至りませんでした。<br>
 <a href="https://nisetexture.com/#archive/hyperlog" target="_blank">展覧会「hyperLOG」</a>で展示した<a href="https://nisetexture.com/#archive/LRTK1" target="_blank">『リビングルーム・トレーニング キッチン#1』</a>を応募しました。
@@ -102,11 +133,11 @@ text_credit: ``,
       text_en: `I submitted an entry to the Yamanashi Media Arts Award 2025-26, but unfortunately it was not selected. I submitted<a href="https://nisetexture.com/#archive/LRTK1" target="_blank"> [Living Room Training Kitchen #1]</a>, which was exhibited at <a href="https://nisetexture.com/#archive/hyperlog" target="_blank">the [hyperLOG] exhibition</a>. I am making the submission materials publicly available.<br>
 English translation by AI`,
 
-       links: {
+      links: {
         "提出資料": "https://5884017.notion.site/nisetexture",
-  }
-},
-{
+      }
+    },
+    {
       id: "psemac2025",
       title: `【不採択】令和7年度国内クリエイター発表支援プログラム`,
       title_en: "[Rejected]Project to Support Emerging Media Arts Creators",
@@ -116,26 +147,26 @@ English translation by AI`,
       media: [
         "workimg/competition/competition_samune-no_a.png",
         "https://youtu.be/ZJcfGl9oPQs",
-         "workimg/competition/competition_samune-no_a.png",
-        
+        "workimg/competition/competition_samune-no_a.png",
+
       ],
       captions: [
-    "　",
-    "自己PR動画"
-    
-  ],
+        "　",
+        "自己PR動画"
+
+      ],
       text_jp: `文化庁メディア芸術クリエイター育成支援事業における、国内クリエイター発表支援プログラムに「『リビングルーム・トレーニング』発表プロジェクト」を応募しました。
 残念ながら採択には至りませんでした。`,
-text_en: `I submitted “Living Room Training Presentation Project”  to the Domestic Creator Presentation Support Program of the Agency for Cultural Affairs’ Media Arts Creator Development Support Project.
+      text_en: `I submitted “Living Room Training Presentation Project”  to the Domestic Creator Presentation Support Program of the Agency for Cultural Affairs’ Media Arts Creator Development Support Project.
 Unfortunately, it was not selected.`,
 
-       links: {
+      links: {
         "企画書": "https://docs.google.com/presentation/d/1P6K32BFetL3x3mNxTTieWASZGmA7460r-tqYmnIBzEA/edit?usp=sharing",
-    "Website": "https://creators.j-mediaarts.bunka.go.jp/2025-entry"
-   
-  }
-},
-{
+        "Website": "https://creators.j-mediaarts.bunka.go.jp/2025-entry"
+
+      }
+    },
+    {
       id: "hyperlog",
       title: "hyperLOG",
       title_en: "hyperLOG",
@@ -150,25 +181,25 @@ Unfortunately, it was not selected.`,
         "https://youtu.be/FvWRGIJsiaE?si=FP_535e-OSIgRdKp"
       ],
       captions: [
-    "リビングルーム・トレーニング#1＆構造図",
-    "リビングルーム・トレーニング#1",
-    "リビングルーム・トレーニング#1",
-    "リビングルーム・トレーニング#1構造図",
-    "配信『「リビングルーム・トレーニング キッチン#1」を見る』"
-    
-  ],
+        "リビングルーム・トレーニング#1＆構造図",
+        "リビングルーム・トレーニング#1",
+        "リビングルーム・トレーニング#1",
+        "リビングルーム・トレーニング#1構造図",
+        "配信『「リビングルーム・トレーニング キッチン#1」を見る』"
+
+      ],
       text_jp: `<a href="https://okoshimadoka.com/about">大越円香</a>キュレーションにて、apploitで開催されたグループ展。
 映像の展示、作品構造図の展示、作品を別の場所から見るという配信をしました。`,
-text_en: `A group exhibition curated by <a href="https://okoshimadoka.com/about">Okoshi Madoka</a> and held at applaudit.
+      text_en: `A group exhibition curated by <a href="https://okoshimadoka.com/about">Okoshi Madoka</a> and held at applaudit.
 nisetexture presented a video installation, an exhibition of the work’s structural diagram, and a live stream that allowed the work to be viewed from a different location.`,
-text_credit: `テクニカルサポート：<br>嶋晏澄<br>樋口聡一郎`,
-       links: {
-    "Website": "https://aaploit.com/hyperLOG",
-   "place": "https://aaploit.com/",
-   
-  }
-},
- {
+      text_credit: `テクニカルサポート：<br>嶋晏澄<br>樋口聡一郎`,
+      links: {
+        "Website": "https://aaploit.com/hyperLOG",
+        "place": "https://aaploit.com/",
+
+      }
+    },
+    {
       id: "hikari",
       title: "光の方へ",
       title_en: "Towards the Light",
@@ -177,7 +208,7 @@ text_credit: `テクニカルサポート：<br>嶋晏澄<br>樋口聡一郎`,
       place: "tokyo,moto-eigakan",
       media: [
         "workimg/hikari/hikarinohouhe1.jpg",
-      "workimg/hikari/hikarinohouhe2.jpg",
+        "workimg/hikari/hikarinohouhe2.jpg",
         "workimg/hikari/hikarinohouhe3.jpg",
         "workimg/hikari/hikarinohouhe4.jpg",
         "workimg/hikari/hikarinohouhe5.jpg",
@@ -186,34 +217,34 @@ text_credit: `テクニカルサポート：<br>嶋晏澄<br>樋口聡一郎`,
         "workimg/hikari/hikarinohouhe8.jpg",
         "https://youtube.com/live/CDs1cfrYl-s"
       ],
-           captions: [
-    "メインビジュアル",
-    "会場写真",
-    "会場写真",
-    "会場写真",
-    "会場写真",
-    "会場写真",
-    "リビングルーム・トレーニング：ケンタッキーフライドチキンウインターパック10ピース",
-    "リビングルーム・トレーニング：ケンタッキーフライドチキンウインターパック10ピース",
-    "配信「リビングルーム・トレーニング　五目焼きそば、ビール（録音）」"
-  ],
-     text_jp: `ニセテクスチャと<a href="https://kenjimonden.com/">門田健嗣</a>によるグループ展。<br>ニセテクスチャは生活の行為を取り出し、パフォーマンスへ変換し都市や街に再配置た作品を展示しました。それを撮影という行為で一方的な視線として映すことで、繰り返し続く閉じた環境を外側から描き出すことを試みます。<br>門田健嗣は、建築的構築性を持つ物体と映像が交錯し、相互に影響し合いながら構築される共生環境に関心を持ち、映像に「光らしさ」を求めることで、これらを生成する手法を模索します。
+      captions: [
+        "メインビジュアル",
+        "会場写真",
+        "会場写真",
+        "会場写真",
+        "会場写真",
+        "会場写真",
+        "リビングルーム・トレーニング：ケンタッキーフライドチキンウインターパック10ピース",
+        "リビングルーム・トレーニング：ケンタッキーフライドチキンウインターパック10ピース",
+        "配信「リビングルーム・トレーニング　五目焼きそば、ビール（録音）」"
+      ],
+      text_jp: `ニセテクスチャと<a href="https://kenjimonden.com/">門田健嗣</a>によるグループ展。<br>ニセテクスチャは生活の行為を取り出し、パフォーマンスへ変換し都市や街に再配置た作品を展示しました。それを撮影という行為で一方的な視線として映すことで、繰り返し続く閉じた環境を外側から描き出すことを試みます。<br>門田健嗣は、建築的構築性を持つ物体と映像が交錯し、相互に影響し合いながら構築される共生環境に関心を持ち、映像に「光らしさ」を求めることで、これらを生成する手法を模索します。
               <br><br>
               「光の方へ」というタイトルにあるように、映像の要素である「光」に対して、それぞれのアプローチで表現を試みます。そこには、生活の中で行われる作業や住居などのモチーフが、制作を通じて洗練され、変形しながら浮かび上がります。映像を単なる記録メディアではなく、それぞれのモチーフが光として表出するような作品を展示しました。`,
-text_en: `A group exhibition by nisetexture and <a href="https://kenjimonden.com/">Monden Kenji</a>.
+      text_en: `A group exhibition by nisetexture and <a href="https://kenjimonden.com/">Monden Kenji</a>.
           Nisetexture exhibited works that extracted acts of daily life, transformed them into performances, and repositioned them within cities and streets. By capturing these through the act of photography as a one-sided gaze, it attempts to depict the closed, repetitive environment from the outside.<br>
           Kenshi Kadota explores symbiotic environments where architecturally constructed objects and moving images intersect and mutually influence each other. By seeking “light-like qualities” in his footage, he investigates methods to generate these environments.<br><br>
 
           As suggested by the title “Towards the Light,” each artist attempts expression through their own approach to “light,” an element of video. Within this, motifs such as tasks performed in daily life and dwellings emerge, refined and transformed through the creative process. The exhibition featured works where video was not merely a recording medium, but where each motif manifested as light.`,
-text_credit: `デザイン：<a href="https://arata-new.jp">新</a>`,
-       links: {
-    "Website": "https://towards-the-light.notion.site/",
-    "place": "https://www.moto-eigakan.com/",
-    
-    "design": "https://arata-new.jp/"
-  }
-},
-{
+      text_credit: `デザイン：<a href="https://arata-new.jp">新</a>`,
+      links: {
+        "Website": "https://towards-the-light.notion.site/",
+        "place": "https://www.moto-eigakan.com/",
+
+        "design": "https://arata-new.jp/"
+      }
+    },
+    {
       id: "psemac2024",
       title: "【不採択】令和6年度国内クリエイター発表支援プログラム",
       title_en: "[Rejected]Project to Support Emerging Media Arts Creators",
@@ -225,21 +256,21 @@ text_credit: `デザイン：<a href="https://arata-new.jp">新</a>`,
         "https://youtu.be/Y7xAR7hBGYs?si=em1NhIUqMhf-rT40"
       ],
       captions: [
-    "　",
-    "自己PR動画"
-    
-  ],
+        "　",
+        "自己PR動画"
+
+      ],
       text_jp: `文化庁メディア芸術クリエイター育成支援事業における、国内クリエイター発表支援プログラムに「オールライト!『クールなループ、生活の成果、』」を応募しました。
 残念ながら採択には至りませんでした。`,
-text_en: `I submitted All Light"Cool Loops, Life Brights," to the Domestic Creator Presentation Support Program of the Agency for Cultural Affairs’ Media Arts Creator Development Support Project.
+      text_en: `I submitted All Light"Cool Loops, Life Brights," to the Domestic Creator Presentation Support Program of the Agency for Cultural Affairs’ Media Arts Creator Development Support Project.
 Unfortunately, it was not selected.`,
 
-       links: {
+      links: {
         "企画書": "https://drive.google.com/file/d/1xfUZTEu-pL9g0SMdFZQ8ivqVSr8nJ34w/view?usp=sharing",
-    "Website": "https://creators.j-mediaarts.bunka.go.jp/news/15226"
-   
-  }
-}
+        "Website": "https://creators.j-mediaarts.bunka.go.jp/news/15226"
+
+      }
+    }
 
   ]
 };
