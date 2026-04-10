812fff0db306632e3dce2a0e2da56899b69d16c1
daizu28 <kinakodaizukinako@gmail.com>
2026-04-10 23:08:28 +0900

    blog004_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "004",
+    category: "sleep soundly 安心して眠る",
+    title: '#1　安心して眠る方法',
+    writer: "オオタソラ",
+    date: "2026-04-11",
+    tag: ["連載"],
+    samune: "../blog_img/sleep-soundly/sleepSoundly_samune.webp",
+  },
+  {
     id: "003",
     category: "飲み会",
     title: '立春！飲み会の記録',
