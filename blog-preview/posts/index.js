2fc0836a7bbba7f457b94bc6a95cd3207ea4df5e
daizu28 <kinakodaizukinako@gmail.com>
2026-04-17 23:04:34 +0900

    blog06_プレビュー作成

---
+++ blog-preview/posts/index.js
@@ -0,0 +1,11 @@
+export const posts = [
+  {
+    id: "006",
+    category: "日記",
+    title: 'もらったカギで開ける',
+    writer: "小林玲衣奈",
+    date: "2026-04-20",
+    tag: ["連載", "日記"],
+    samune: "../blog_img/opening-with-a-given-key/thumbnail.webp",
+  }
+];
