3dc91470551362dc388dcaad376e9942e4efa022
daizu28 <kinakodaizukinako@gmail.com>
2026-04-19 23:02:37 +0900

    blog06_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,7 +2,16 @@
 export const posts = [
   // 新しいのは上に記入
   {
- id: "005",
+    id: "006",
+    category: "日記",
+    title: 'もらったカギで開ける',
+    writer: "小林玲衣奈",
+    date: "2026-04-20",
+    tag: ["連載", "日記"],
+    samune: "../blog_img/opening-with-a-given-key/thumbnail.webp",
+  },
+  {
+    id: "005",
     category: "日記",
     title: 'こぼれちゃうよ',
     writer: "新",
