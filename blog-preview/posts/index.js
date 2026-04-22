bc191e50c3e989d3f469bbf4b35273f64d7c4dab
daizu28 <kinakodaizukinako@gmail.com>
2026-04-23 01:21:33 +0900

    blog07_プレビュー追加

---
+++ blog-preview/posts/index.js
@@ -0,0 +1,11 @@
+export const posts = [
+  {
+    id: "007",
+    category: "ぬるい言葉マップ",
+    title: '第1回　編集のまわりから',
+    writer: "小林玲衣奈",
+    date: "2026-04-25",
+    tag: ["連載"],
+    samune: "　",
+  }
+];
