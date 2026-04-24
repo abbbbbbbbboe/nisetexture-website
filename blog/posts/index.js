798bf87803b4b015a44aa8fa219b2ab5040d3ba5
daizu28 <kinakodaizukinako@gmail.com>
2026-04-24 23:29:05 +0900

    blog07_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "007",
+    category: "ぬるい言葉マップ",
+    title: '第1回　編集のまわりから',
+    writer: "小林玲衣奈",
+    date: "2026-04-25",
+    tag: ["連載"],
+    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+  },
+  {
     id: "006",
     category: "日記",
     title: 'もらったカギで開ける',
