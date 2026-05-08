b819221c72af582dedce46414a4be5f7c94a06be
otasora <ootaoota0038@gmail.com>
2026-05-08 10:34:09 +0900

    blog08_タイトルに全角スペース修正

--- blog-preview/posts/index.js
+++ blog-preview/posts/index.js
@@ -2,7 +2,7 @@ export const posts = [
   {
      id: "008",
     category: "sleep soundly 安心して眠る",
-    title: '#2 マツボックリ　イン　タイホウ',
+    title: '#2　マツボックリ　イン　タイホウ',
     writer: "オオタソラ",
     date: "2026-05-09",
     tag: ["連載"],
