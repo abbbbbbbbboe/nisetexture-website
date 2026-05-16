76b79d038513e944dec0313b456590cbceb5b16e
otasora <ootaoota0038@gmail.com>
2026-05-17 00:52:15 +0900

    blog10_プレビュー

---
+++ blog-preview/posts/index.js
@@ -0,0 +1,11 @@
+export const posts = [
+  {
+    id: "010",
+    category: "日記",
+    title: 'もらったカギで開ける',
+    writer: "オオタソラ",
+    date: "2026-05-18",
+    tag: ["連載", "日記"],
+    samune: "../blog_img/opening-with-a-given-key/thumbnail.webp",
+  }
+];
