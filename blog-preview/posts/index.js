3c4b8d84e8f45e6f63ef90bd354c2eca0cee88bf
daizu28 <kinakodaizukinako@gmail.com>
2026-05-19 22:02:22 +0900

    blog11_プレビュー追加

---
+++ blog-preview/posts/index.js
@@ -0,0 +1,11 @@
+export const posts = [
+  {
+ id: "011",
+    category: "ぬるい言葉マップ",
+    title: '第2回　世界と時間を持っている',
+    writer: "小林玲衣奈",
+    date: "2026-05-23",
+    tag: ["連載"],
+    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+  }
+];
