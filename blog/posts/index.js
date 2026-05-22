8059356975917051a668991ffcdddcb220230701
daizu28 <kinakodaizukinako@gmail.com>
2026-05-22 23:19:24 +0900

    blog011_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "011",
+    category: "ぬるい言葉マップ",
+    title: '第2回　世界と時間を持っている',
+    writer: "小林玲衣奈",
+    date: "2026-05-23",
+    tag: ["連載"],
+    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+  },
+  {
     id: "010",
     category: "日記",
     title: 'もらったカギで開ける',
