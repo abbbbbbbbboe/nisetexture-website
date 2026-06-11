9711faf79499cde99dcd2d70837aab9757000dfb
otasora <ootaoota0038@gmail.com>
2026-06-11 23:39:49 +0900

    blog13_プレビュー修正

--- blog-preview/posts/index.js
+++ blog-preview/posts/index.js
@@ -2,7 +2,7 @@ export const posts = [
   {
     id: "013",
      category: "sleep soundly 安心して眠る",
-    title: '#3 あなたの馬',
+    title: '#3　あなたの馬',
     writer: "オオタソラ",
     date: "2026-06-13",
     tag: ["連載"],
