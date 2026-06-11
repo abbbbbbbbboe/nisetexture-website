2055d8ec094a059af639c76da34779ed57546022
otasora <ootaoota0038@gmail.com>
2026-06-11 10:32:08 +0900

    blog13_プレビュー追加

---
+++ blog-preview/posts/index.js
@@ -0,0 +1,11 @@
+export const posts = [
+  {
+    id: "013",
+     category: "sleep soundly 安心して眠る",
+    title: '#3 あなたの馬',
+    writer: "オオタソラ",
+    date: "2026-06-13",
+    tag: ["連載"],
+    samune: "../blog_img/sleep-soundly/sleepSoundly_samune.webp",
+  }
+];
