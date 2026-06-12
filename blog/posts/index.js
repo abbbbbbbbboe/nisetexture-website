fcde88bff3f5c8b98d108ebaae7bc1dccb4c6c51
otasora <ootaoota0038@gmail.com>
2026-06-13 01:26:23 +0900

    blog13_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "013",
+     category: "sleep soundly 安心して眠る",
+    title: '#3　あなたの馬',
+    writer: "オオタソラ",
+    date: "2026-06-13",
+    tag: ["連載"],
+    samune: "../blog_img/sleep-soundly/sleepSoundly_samune.webp",
+  },
+  {
     id: "012",
     category: "特集",
     title: '音のストリートビュー 【sound traces】について',
