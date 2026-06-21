f642c5b1a627e9e3f16d670d685cf9a318d6801c
daizu28 <kinakodaizukinako@gmail.com>
2026-06-22 01:44:33 +0900

    blog15_リンク修正

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -8,7 +8,7 @@ export const posts = [
     writer: "小林玲衣奈",
     date: "2026-06-22",
     tag: ["連載"],
-    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+    samune: "../blog_img/opening-with-a-given-key/thumbnail.webp",
   },
   {
     id: "014",
