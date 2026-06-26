df65bfe774e5b680717ede7fad930820e8664652
daizu28 <kinakodaizukinako@gmail.com>
2026-06-26 23:22:58 +0900

    blog16_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
     {
+    id: "016",
+    category: "ぬるい言葉マップ",
+    title: '第3回　見えない・聞こえない・分からない',
+    writer: "小林玲衣奈",
+    date: "2026-06-27",
+    tag: ["連載"],
+    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+  },
+    {
     id: "015",
     category: "日記",
     title: 'もらったカギで開ける',
