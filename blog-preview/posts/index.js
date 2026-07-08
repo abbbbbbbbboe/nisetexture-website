77a09cef0a7f577f9860b23419dc8ad9151ea438
otasora <ootaoota0038@gmail.com>
2026-07-08 09:01:15 +0900

    blog17_プレビュー

---
+++ blog-preview/posts/index.js
@@ -0,0 +1,11 @@
+export const posts = [
+  {
+    id: "017",
+    category: "sleep soundly 安心して眠る",
+    title: '#4　馬魂碑',
+    writer: "オオタソラ",
+    date: "2026-07-11",
+    tag: ["連載"],
+    samune: "../blog_img/sleep-soundly/sleepSoundly_samune.webp",
+  }
+];
