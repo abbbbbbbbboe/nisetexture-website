6e7cef3ca8beb30c404ed07e135de79b8fd2e094
otasora <ootaoota0038@gmail.com>
2026-07-20 00:06:04 +0900

    blog19_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "019",
+    category: "日記",
+    title: 'もらったカギで開ける',
+    writer: "オオタソラ",
+    date: "2026-07-20",
+    tag: ["連載", "日記"],
+    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+  },
+  {
     id: "018",
     category: "日記",
     title: 'こぼれちゃうよ',
