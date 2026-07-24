fdd48f760092c97846356972e34c7d2609588e6e
daizu28 <kinakodaizukinako@gmail.com>
2026-07-24 23:30:50 +0900

    blog20_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "020",
+    category: "ぬるい言葉マップ",
+    title: '第4回　テレプレゼンス',
+    writer: "小林玲衣奈",
+    date: "2026-07-25",
+    tag: ["連載"],
+    samune: "../blog_img/thinking-in-lukewarm-words/thumbnail2_tlw.webp",
+  },
+  {
     id: "019",
     category: "日記",
     title: 'もらったカギで開ける',
