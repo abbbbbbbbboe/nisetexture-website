6eb172c1ac9dca6309f40616d42ace384f7c6255
otasora <ootaoota0038@gmail.com>
2026-08-07 23:52:08 +0900

    blog21_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "021",
+    category: "sleep soundly 安心して眠る",
+    title: '#5　訓練された馬',
+    writer: "オオタソラ",
+    date: "2026-08-08",
+    tag: ["連載"],
+    samune: "../blog_img/sleep-soundly/sleepSoundly_samune.webp",
+  },
+  {
     id: "020",
     category: "ぬるい言葉マップ",
     title: '第4回　テレプレゼンス',
