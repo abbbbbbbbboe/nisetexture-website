599fa93840aaa67eb42650a8b1e845d99c516007
daizu28 <kinakodaizukinako@gmail.com>
2026-08-16 23:17:24 +0900

    blog22_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+      id: "022",
+    category: "日記",
+    title: 'もらったカギで開ける',
+    writer: "小林玲衣奈",
+    date: "2026-08-17",
+    tag: ["連載", "日記"],
+    samune: "../blog_img/opening-with-a-given-key/thumbnail.webp",
+  },
+  {
     id: "021",
     category: "sleep soundly 安心して眠る",
     title: '#5　訓練された馬',
