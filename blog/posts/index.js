5e1df19e4be397b3bd07e07f1a51db5447499cc8
daizu28 <kinakodaizukinako@gmail.com>
2026-09-13 23:00:25 +0900

    blog25_公開

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,15 @@
 export const posts = [
   // 新しいのは上に記入
   {
+    id: "025",
+    category: "日記",
+    title: 'こぼれちゃうよ',
+    writer: "新",
+    date: "2026-09-14",
+    tag: ["連載", "日記"],
+    samune: "../blog_img/spillover/thumbnail.webp",
+  },
+  {
     id: "024",
     category: "日記",
     title: 'こぼれちゃうよ',
